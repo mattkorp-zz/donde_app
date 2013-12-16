22ace45b5a4e031aab8b4dac48808468f504589f
Matthew Korporaal <matthew.korporaal@gmail.com>
2013-12-15 20:35:14 -0500

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DondeApp::Application.config.secret_token = '0c505d625ca1e3e1c27b9e91f19fc6f4f5a5ce3adb3c296d34c3eee665e5bdc0856de3ed89a684452af9cd6b59b3710d0ce9bb10ac80f8c68c5d55bde7e3caf8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DondeApp::Application.config.session_store :cookie_store, key: '_donde_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DondeApp::Application.config.session_store :active_record_store
